7cf0c08e9b39057ceea092a738d3a51650633ba8
TzuYuan (Andrew) Wang <andrew970314@gmail.com>
2021-05-19 16:33:14 -0400

    create clean data function

--- blog/js/septaBusLocation.js
+++ blog/js/septaBusLocation.js
@@ -1,9 +1,27 @@
+var routes;
+var locationData;
+var markers;
+
+var cleanData = function(data){
+    locationData = data.routes[0];
+    routes = Object.keys(data.routes[0]) // create an array of routes
+    routes.forEach(function(route){                     // add routes to location data
+        locationData[route].forEach(function(data){
+            data.route = route
+        })
+    })
+}
+
+
 $.ajax({
     url: "https://www3.septa.org/hackathon/TransitViewAll/",
   
    dataType: "jsonp",
     success: function(data){
-      testData = data;
+      cleanData(data);
     }
   }); 
-  testData.routes[0]["BLVDDIR"]
+  //testData.routes[0]["BLVDDIR"]
+
+
+  // use two for each to create marker 
